c0819e81b99d8ebaf7bd9d617bb9eae5ca68657c
anupamac <anupamac@nvidia.com>
2025-02-27 14:38:00 -0800

    Set default to MaxRecords, fix bug with metadata

--- lib/DXIL/DxilMetadataHelper.cpp
+++ lib/DXIL/DxilMetadataHelper.cpp
@@ -2822,7 +2822,7 @@ DxilMDHelper::EmitDxilNodeIOState(const hlsl::NodeIOProperties &Node) {
 
     if (Node.MaxRecordsPerNode) {    
       MDVals.emplace_back(
-              Uint32ToConstMD(DxilMDHelper::kDxilNodeMaxRecordsSharedWithTag));
+              Uint32ToConstMD(DxilMDHelper::kDxilNodeMaxRecordsPerNodeTag));
       MDVals.emplace_back(Uint32ToConstMD(Node.MaxRecordsPerNode));
     }
 
